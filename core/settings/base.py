1f4e481088bd020900d2ab2e75d4aed1b65cdd4d
Asadbek Sobirjonov <88094901+sobirjonovme@users.noreply.github.com>
2023-07-17 01:48:51 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -59,7 +59,10 @@ THIRD_PARTY_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.SessionAuthentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
