46e6a24a3da230ad9d22e8412c29a1f8bba560a1
Asadbek Sobirjonov <88094901+sobirjonovme@users.noreply.github.com>
2023-07-17 00:52:13 +0500

    Update production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -15,7 +15,15 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 # CSRF_TRUSTED_ORIGINS = ["https://example.com"]
-CSRF_TRUSTED_ORIGINS = ["https://", "http://"]
+# CSRF_TRUSTED_ORIGINS = ["https://", "http://"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost",
+    "https://localhost",
+    "http://127.0.0.1",
+    "https://127.0.0.1",
+    "https://pythonanywhere.com",
+]
+
 
 ###################################################################
 # CORS
