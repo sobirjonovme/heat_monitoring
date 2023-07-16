d202f7a3168fefc8782fa1b46ef5b7153ee6c953
Asadbek <huntsman6004@gmail.com>
2023-07-16 23:05:31 +0500

    feat: sqlite database added to settings.develop

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -2,3 +2,10 @@ from .base import *  # noqa
 
 DEBUG = True
 CELERY_TASK_ALWAYS_EAGER = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
