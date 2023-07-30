55e7ef362d772c4128b9a9951fb739bcd3100090
Asadbek <huntsman6004@gmail.com>
2023-07-30 19:07:07 +0500

    feat: custom Login API added

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from .api_endpoints import MyProfileAPIView
+from .api_endpoints import CustomTokenObtainPairView, MyProfileAPIView
 
 app_name = "users"
 
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token-obtain-pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("profile/", MyProfileAPIView.as_view(), name="my-profile"),
 ]
