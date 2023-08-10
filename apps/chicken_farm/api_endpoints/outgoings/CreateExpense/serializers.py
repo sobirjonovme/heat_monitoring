0f16d247af82b517179843200c22cb89f898e1c3
Asadbek <huntsman6004@gmail.com>
2023-08-11 00:04:15 +0500

    fix: CreateExpense API changed

--- apps/chicken_farm/api_endpoints/outgoings/CreateExpense/serializers.py
+++ apps/chicken_farm/api_endpoints/outgoings/CreateExpense/serializers.py
@@ -5,21 +5,25 @@ from apps.chicken_farm.serializers import FarmExpenseTypeSerializer
 
 
 class CreateFarmExpenseSerializer(serializers.ModelSerializer):
-    type = FarmExpenseTypeSerializer()
+    type = FarmExpenseTypeSerializer(read_only=True)
+    type_name = serializers.CharField(write_only=True, max_length=255, required=True)
 
     class Meta:
         model = FarmExpense
         fields = (
-            "type",
             "item_amount",
             "card_payment",
             "cash_payment",
             "debt_payment",
             "image",
+            # read only fields
+            "type",
+            # write only fields
+            "type_name",
         )
 
     def create(self, validated_data):
-        expense_type = validated_data.pop("type")
-        expense_type_obj = FarmExpenseType.objects.create(**expense_type)
+        type_name = validated_data.pop("type_name")
+        expense_type_obj = FarmExpenseType.objects.create(name=type_name)
         farm_expense = FarmExpense.objects.create(type=expense_type_obj, **validated_data)
         return farm_expense
