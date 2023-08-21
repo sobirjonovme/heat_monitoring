5df7720c12c1a0c4ef05e61774f15a3032fe5145
Asadbek Sobirjonov <huntsman6004@gmail.com>
2023-08-21 19:18:10 +0500

    fix: managers changed

--- apps/chicken_farm/api_endpoints/common/HelperView/views.py
+++ apps/chicken_farm/api_endpoints/common/HelperView/views.py
@@ -1,30 +1,32 @@
-from django.db import models
+# from django.db import models
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.chicken_farm.models import FarmDailyReport, FarmSalesReport
-from apps.chicken_farm.utils.daily_reports_update import bulk_update_daily_reports
+
+# from apps.chicken_farm.utils.daily_reports_update import \
+#     bulk_update_daily_reports
 
 
 class HelperAPIView(APIView):
     def get(self, request, format=None):
-        sales_reports = FarmDailyReport.objects.all().order_by("date")
-        target = sales_reports[1]
-        print(target)
+        # sales_reports = FarmDailyReport.objects.all().order_by("date")
+        # target = sales_reports[1]
+        # print(target)
         a = FarmSalesReport.objects.all()
         # bulk_update_daily_reports(target)
 
-        print(f'\n{a.values_list("sold_egg_boxes", flat=True)}\n')
-        a = models.Sum(a.values_list("sold_egg_boxes", flat=True))
-        print(f"\n\n\tSold egg boxes: {a}\n\n")
+        # print(f'\n{a.values_list("sold_egg_boxes", flat=True)}\n')
+        # a = models.Sum(a.values_list("sold_egg_boxes", flat=True))
+        # print(f"\n\n\tSold egg boxes: {a}\n\n")
         print("\n\nbefore annotate\n\n")
-        sales_reports = sales_reports.annotate_sold_egg_boxes()
+        daily_reports = FarmDailyReport.objects.all().annotate_sold_egg_boxes()
         print("\n\nafter annotate\n\n")
-        print(sales_reports)
+        print(daily_reports)
         print("\n\nbefore for\n\n")
-        for a in sales_reports:
-            print(a.sold_egg_boxes1)
+        for a in daily_reports:
+            print(a.cnt)
 
         return Response(status=status.HTTP_200_OK)
 
--- apps/chicken_farm/models/managers.py
+++ apps/chicken_farm/models/managers.py
@@ -6,12 +6,25 @@ class DailyReportQuerySet(models.QuerySet):
     def annotate_sold_egg_boxes(self):
         """Annotate sales_report_count field to queryset."""
         sales_report_model = apps.get_model("chicken_farm", "FarmSalesReport")
-        sub_query = sales_report_model.objects.filter(sold_at__date=models.OuterRef("date"))
-        sub_query = sub_query.aggregate(models.Sum("sold_egg_boxes"))
 
-        return self.annotate(
-            sold_egg_boxes1=models.Value(sub_query["sold_egg_boxes__sum"], output_field=models.IntegerField())
+        # B_subquery = (
+        #     sales_report_model.objects.filter(sold_at__date=models.OuterRef("date")).
+        #     aggregate(summ=models.Sum("sold_egg_boxes"))["summ"]
+        # )
+        B_subquery = (
+            sales_report_model.objects.filter(sold_at__date=models.OuterRef("date"))
+            .values("sold_at__date")
+            .annotate(summ=models.Sum("sold_egg_boxes"))
+            .values("summ")
         )
+        return self.annotate(cnt=models.Subquery(B_subquery))
+
+        # sub_query = sales_report_model.objects.filter(sold_at__date=models.OuterRef("date"))
+        # sub_query = sub_query.aggregate(models.Sum("sold_egg_boxes"))
+        #
+        # return self.annotate(
+        #     sold_egg_boxes1=models.Value(sub_query["sold_egg_boxes__sum"], output_field=models.IntegerField())
+        # )
 
 
 class DailyReportManager(models.Manager.from_queryset(DailyReportQuerySet)):  # type: ignore
