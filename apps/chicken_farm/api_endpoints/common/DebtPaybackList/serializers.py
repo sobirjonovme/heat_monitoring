c5c33137f25e92331560683bbf69da15e218533f
Asadbek Sobirjonov <huntsman6004@gmail.com>
2023-08-22 10:22:45 +0500

    feat: DebtPayback list changed

--- apps/chicken_farm/api_endpoints/common/DebtPaybackList/serializers.py
+++ apps/chicken_farm/api_endpoints/common/DebtPaybackList/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
 
-from apps.chicken_farm.models import FarmDebtPayback
+from apps.chicken_farm.models import (FarmDebtPayback, FarmExpense,
+                                      FarmSalesReport)
+
+
+class SalesReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FarmSalesReport
+        fields = (
+            "id",
+            "comment",
+            "sold_at",
+        )
+        ref_name = "FarmDebtPaybackSalesReportSerializer"
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    type_name = serializers.CharField(source="type.name", read_only=True)
+
+    class Meta:
+        model = FarmExpense
+        fields = (
+            "id",
+            "type_name",
+            "item_amount",
+            "image",
+            "date",
+        )
+        ref_name = "FarmDebtPaybackExpenseSerializer"
 
 
 class FarmDebtPaybackListSerializer(serializers.ModelSerializer):
+    sales_report = SalesReportSerializer(read_only=True)
+    expense = ExpenseSerializer(read_only=True)
+
     class Meta:
         model = FarmDebtPayback
         fields = (
--- apps/chicken_farm/api_endpoints/common/DebtPaybackList/views.py
+++ apps/chicken_farm/api_endpoints/common/DebtPaybackList/views.py
@@ -9,12 +9,16 @@ from .serializers import FarmDebtPaybackListSerializer
 
 
 class DebtPaybackListAPIView(ListAPIView):
-    queryset = FarmDebtPayback.objects.all().order_by("-paid_at")
+    queryset = FarmDebtPayback.objects.all()
     serializer_class = FarmDebtPaybackListSerializer
     permission_classes = (IsFarmCounterOrAdmin,)
 
     filter_backends = (DjangoFilterBackend,)
     filterset_class = DebtPaybackFilter
+
+    def get_queryset(self):
+        queryset = self.queryset.order_by("-paid_at")
+        return queryset.select_related("sales_report", "expense", "expense__type")
 
 
 __all__ = ["DebtPaybackListAPIView"]
