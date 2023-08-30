c87081d59cb263aa310ef98b8c67edcee4ba153d
Asadbek <huntsman6004@gmail.com>
2023-08-31 00:32:50 +0500

    feat: expense_category added to CreateFarmExpenseSerializer

--- apps/chicken_farm/api_endpoints/outgoings/CreateExpense/serializers.py
+++ apps/chicken_farm/api_endpoints/outgoings/CreateExpense/serializers.py
@@ -8,6 +8,7 @@ from apps.chicken_farm.serializers import FarmExpenseTypeSerializer
 class CreateFarmExpenseSerializer(serializers.ModelSerializer):
     type = FarmExpenseTypeSerializer(read_only=True)
     type_name = serializers.CharField(write_only=True, max_length=255, required=True)
+    expense_category = serializers.ChoiceField(write_only=True, choices=FarmExpenseCategory.choices, required=True)
 
     class Meta:
         model = FarmExpense
@@ -19,16 +20,18 @@ class CreateFarmExpenseSerializer(serializers.ModelSerializer):
             "debt_payment",
             "comment",
             "image",
-            # read only fields
+            # # read only fields
             "type",
             # write only fields
             "type_name",
+            "expense_category",
         )
 
     def create(self, validated_data):
         type_name = validated_data.pop("type_name")
+        expense_category = validated_data.pop("expense_category")
         expense_type_obj, _ = FarmExpenseType.objects.get_or_create(
-            name=type_name, defaults={"category": FarmExpenseCategory.OTHER}
+            name=type_name, defaults={"category": expense_category}
         )
         farm_expense = FarmExpense.objects.create(type=expense_type_obj, **validated_data)
         return farm_expense
